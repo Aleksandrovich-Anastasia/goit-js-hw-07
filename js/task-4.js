164e7d1717017b9281f1e88b196988fbd6562e90
Anastasia <aleksandrovich2109@gmail.com>
2025-06-22 13:37:37 +0300

    hw-7

--- js/task-4.js
+++ js/task-4.js
@@ -1,45 +1,24 @@
-const getTotalBalanceByGender = (users, gender) => users
-  .filter(user => user.gender === gender)
-  .reduce((total, user) => total + user.balance, 0);
-  const clients = [
-    {
-      name: "Moore Hensley",
-      gender: "male",
-      balance: 2811
-    },
-    {
-      name: "Sharlene Bush",
-      gender: "female",
-      balance: 3821
-    },
-    {
-      name: "Ross Vazquez",
-      gender: "male",
-      balance: 3793
-    },
-    {
-      name: "Elma Head",
-      gender: "female",
-      balance: 2278
-    },
-    {
-      name: "Carey Barr",
-      gender: "male",
-      balance: 3951
-    },
-    {
-      name: "Blackburn Dotson",
-      gender: "male",
-      balance: 1498
-    },
-    {
-      name: "Sheree Anthony",
-      gender: "female",
-      balance: 2764
-    }
-  ];
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
   
-  console.log(getTotalBalanceByGender(clients, "male")); // 12053
-  
-  console.log(getTotalBalanceByGender(clients, "female")); // 8863
-  
+  const { email, password } = event.target.elements;
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+
+  if (emailValue === "" || passwordValue === "") {
+    alert("All form fields must be filled in");
+    return;
+  }
+
+  const formData = {
+    email: emailValue,
+    password: passwordValue,
+  };
+
+  console.log(formData);
+  event.target.reset();
+}
